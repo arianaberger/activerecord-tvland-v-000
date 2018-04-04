ca9a21ac259f325512af34fefba1355d231c98be
Ariana Berger <ariana.berger@gmail.com>
2018-04-04 15:51:47 +0000

    automatically backed up by learn

--- db/migrate/008_add_attributes_to_shows.rb
+++ db/migrate/008_add_attributes_to_shows.rb
@@ -2,7 +2,7 @@ class AddAttributesToShows < ActiveRecord::Migration[4.2]
   def change
     add_column :shows, :genre, :string
     add_column :shows, :day, :string
-    add_column :shows, :season:, :string
-    
+    add_column :shows, :season, :string
+
 
 end
